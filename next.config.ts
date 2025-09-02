3b7ad206fbd75fb3f44b6e504a24a76ed8f6cc38
mohamed abdelsalam <salamony4all@gmail.com>
2025-09-02 23:17:20 +0000

    File changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type {NextConfig} from 'next';
  
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/alshaya-enterprises',
-  assetPrefix: '/alshaya-enterprises/',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
